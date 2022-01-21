a570572fe12c13a77cb8f387133d9ee689b99a50
Davi <davigomes.ti.ads@gmail.com>
2022-01-21 08:39:11 -0300

    Primeiro teste MVC: Department

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -47,7 +47,7 @@ namespace SalesWebMvc {
             app.UseMvc(routes => {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Departments}/{action=Index}/{id?}");
             });
         }
     }
